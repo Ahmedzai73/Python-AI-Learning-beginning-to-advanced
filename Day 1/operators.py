67cc37467a85dc945669e237cd811250495f52ec
Ahmed Zai <ahmedzai73@gmail.com>
2025-02-17 12:18:57 +0500

    Python-AI-Learning-beginning-to-advanced

---
+++ Day 1/operators.py
@@ -0,0 +1,29 @@
+
+# Integer variables for simple calculations
+num1: int = 50
+num2: int = 60
+num3: int = 70
+
+# Float variables for floating-point calculations
+num4: float = 80
+num5: float = 90
+
+# Calculate the sum of num1, num2, num3, num4, and num5
+total1 = num1 + num2 + num3 + num4 + num5 
+
+# Calculate the difference of num5 and num2
+total2 = num5 - num2
+
+# Calculate the product of num1, num2, num3, and num4
+total3 = num1 * num2 * num3 * num4
+
+# Calculate the quotient of num1, num2, num3, and num4
+total4 = num1 / num2 / num3 / num4 / num5
+
+# Print the results with descriptive messages
+print(f"The sum of the numbers is: {total1}")
+print(f"The difference of the numbers is: {total2}")
+print(f"The product of the numbers is: {total3}")
+print(f"The quotient of the numbers is: {total4:.2f}")  # Round the quotient to 3 decimal places
+ 
+ 
---
+++ Day 1/variables.py
@@ -0,0 +1,4 @@
+user_name = "ahmed"
+user_age = "20"
+
+print(f"Hello, {user_name}! Your age is {user_age}.")
